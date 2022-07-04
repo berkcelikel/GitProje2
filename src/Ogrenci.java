dc4dcc265c29ea9bf880ecfa64db07a56f803d58
berkcelikel <berkcelikel99@gmail.com>
2022-07-04 22:58:48 +0300

    İşlem ikinci başarılı ooldu..

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba");
+
+
+        System.out.println("Giriş başarıı");
+        System.out.println("Selam Dünya");
     }
 }
